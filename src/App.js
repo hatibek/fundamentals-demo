2082aebaa995253dd4729012ca13a150e1f1f705
hatibe <hatibekara@gmail.com>
2024-08-07 21:13:41 -0700

    added navigation

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import TodoList from "./todoList/TodoList";
 import Counter from "./counter/Counter";
 import Calculator from "./calculator/Calculator";
 import RestAPI from "./rest/RestAPI";
+import { Routes, Route, NavLink } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/todo-list">Todo List</NavLink>
+            </li>
+            <li>
+              <NavLink to="/counter">Counter</NavLink>
+            </li>
+            <li>
+              <NavLink to="/calculator">Calculator</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
       <div className="main-grid-container">
-        <div className="grid-item">
-          <TodoList />
-        </div>
-        <div className="grid-item">
-          <Counter />
-        </div>
-        <div className="grid-item">
-          <Calculator />
-        </div>
+        <Routes>
+          <Route path="/todo-list" element={<TodoList />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="*" element={<Calculator />} />
+        </Routes>
       </div>
       <div className="grid-item">
         <RestAPI />
